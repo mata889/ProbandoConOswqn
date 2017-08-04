9d393435b18165593506b872fdb60aff0727dbb3
Oswaldo Varela <oswavare14@hotmail.com>
2017-08-04 07:45:42 -0600

    alojamiento

--- CheqNegro/src/cheqnegro/CheqNegro.java
+++ CheqNegro/src/cheqnegro/CheqNegro.java
@@ -17,6 +17,8 @@ public class CheqNegro {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Oswaldo");
+        System.out.println("gonzalo se la come toda");
+        System.out.println("edw");
     }
     
 }
